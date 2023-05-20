0ba42c28c858c81e3e632aba91f6bcf149f2fcfc
KhaledAlrashidi1 <kalrashi@usc.edu>
2023-05-20 02:46:05 -0700

    adding some description to the the main.cc file
    Signed-off-by: KhaledAlrashidi1 <kalrashi@usc.edu>

--- bazel_examples/example1/main.cc
+++ bazel_examples/example1/main.cc
@@ -1,8 +1,19 @@
+/*
+Program: a basic C++ program that uses bazel
+Author: Khaled Alrashidi
+Date: May 20, 2023
+Description: this is a basic C++ program that outputs strings to 
+             the console. What makes it interesting is that it uses bazel to 
+             build and run the program (check out its BUILD file)
+*/
+
 #include <iostream>
 
 int main() {
 
-    std::cout<<"\nHello World! This is Khaled's learning about C++\n"<<std::endl;
+    std::cout<<"\nHello World! This is Khaled's learning about C++"<<std::endl;
+    std::cout<<"This is the first usage of bazel to run a C++ program!\n"<<std::endl;
+
     return 0;
     
 }
