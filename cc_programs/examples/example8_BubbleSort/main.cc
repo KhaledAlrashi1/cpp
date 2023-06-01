507ec58c3ceff029c49b9662e48ad47af9fb4172
KhaledAlrashidi1 <kalrashi@usc.edu>
2023-05-31 23:37:26 -0700

    documenting examples 8 and 9
    Signed-off-by: KhaledAlrashidi1 <kalrashi@usc.edu>

--- cc_programs/examples/example8_BubbleSort/main.cc
+++ cc_programs/examples/example8_BubbleSort/main.cc
@@ -14,30 +14,35 @@ the integer vector ascendingly (or descenedingly) using bubble sort algorithm.
 
 int main() {
  
+    // declare an integer-type vector and other necessary variables
     std::vector<int> v_input;
     int num_int, temp;
     int ascend_enable = 0;
     int swap_count=0;
 
+    // ask the user to enter the number of integers
     std::cout << "Please enter the number integers: ";
     std::cin >> num_int;
 
+    // ask the user to enter 'n' integers
     std::cout << "Please enter " << num_int << " integers: ";
     for(int i=0; i<num_int; ++i) {
         std::cin >> temp;
         v_input.push_back(temp);
     }
 
+    // ask the user if they want the integers to be sorted ascendingly or descendingly
     std::cout << "Please enter 1 for ascending sort or enter 0 for decending sort: ";
     std::cin  >> ascend_enable;
     std::cout << std::endl;
 
+    // output to the terminal the original vector (before sorting)
     std::cout << "The original input vector: ";
     for(int i=0; i<num_int; ++i){
         std::cout << v_input[i] << " ";
     } std::cout << std::endl << std::endl;
 
-
+    // the bubble sort algorithm (ascend)
     if (ascend_enable == 1) {
         std::cout << "Ascending sort:" << std::endl;
         for(int i=0; i<num_int; ++i){
@@ -51,7 +56,7 @@ int main() {
             }
         }
     }
-
+    // the bubble sort algorithm (descend)
     else if (ascend_enable == 0) {
         std::cout << "decending sort\n" << std::endl;
         for(int i=0; i<num_int; ++i){
@@ -65,18 +70,18 @@ int main() {
             }
         }
     }
-
     else {
         std::cout << "Error: Invalid Input" << std::endl;
         return -1; 
     }
     
-
+    // output the vector (after sorting)
     std::cout << "The input vector after sorting: ";
     for(int i=0; i<num_int; ++i){
         std::cout << v_input[i] << " ";
     } std::cout << std::endl;
 
+    // output the number of swaps
     std::cout << "Number of swaps: " << swap_count << std::endl << std::endl;
 
     return 0;
--- cc_programs/examples/example9_sets/main.cc
+++ cc_programs/examples/example9_sets/main.cc
@@ -14,40 +14,48 @@ int main() {
     std::set<int> int_set;
     int num_int, temp;
 
+    // the begining of the program
     std::cout << "\nHello! This program that does basic set operations ";
     std::cout << "Please note that a set stores only unqiue values\n" << std::endl;
 
+    // ask the user to input the number of intgers they wish to have in the set
     std::cout << "Please enter the number of integers you wish to store in the set: ";
     std::cin >> num_int;
 
+    // ask the user to in 'num_int' integers
     std::cout << "Please enter " << num_int << " integers: ";
     for(int i=0; i<num_int; ++i){
         std::cin >> temp;
         int_set.insert(temp);
     }
 
+    // output the elements stored in the set
     std::cout << "\nPerfect! The elements in the set: ";
     for(auto &e : int_set){
         std::cout << e << " ";
     } std::cout << std::endl;
     
 
+    // ask the user to check if an integer exists in the set
     std::cout << "\nCheck if an integer is in the set" << std::endl;
     std::cout << "Please enter the integer you wish to check: ";
     std::cin >> temp;
-
+    // output to the terminal if the integer exists or not.
     if(int_set.count(temp)) std::cout << "The integer exists" << std::endl << std::endl;
     else std::cout << "The integer doesn't exist" << std::endl << std::endl;
 
 
+    // ask the user to remove an integer from the set (if exist)
     std::cout << "Remove an integer fron the set (if exist)" << std::endl;
     std::cout << "Please enter the integer you wish to remove: ";
     std::cin >> temp;
     std::cout << std::endl;
 
+    // remove the integer (if exist)
     if(int_set.count(temp)){
         int_set.erase(temp);
         std::cout << "Integer was removed" << std::endl;
+        // output the elements stored in the set (after removing an elements)
         std::cout << "\nNow, the elements in the set: ";
         for(auto &e : int_set){
           std::cout << e << " ";
