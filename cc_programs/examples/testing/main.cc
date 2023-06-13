ea40f4b45d56ed9983a20227db2ebd9680753d4e
KhaledAlrashidi1 <kalrashi@usc.edu>
2023-06-13 02:54:59 -0700

    More cpp_programs. blaze test for each program
    Signed-off-by: KhaledAlrashidi1 <kalrashi@usc.edu>

---
+++ cc_programs/examples/testing/main.cc
@@ -0,0 +1,11 @@
+/////////////////////////////////////
+// In this file I try random things.
+/////////////////////////////////////
+
+#include <iostream>
+#include <vector>
+
+int main() {
+
+    return 0
+}
